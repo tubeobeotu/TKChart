58ea34b023a15986d4e0caae509d9ada86afcae5
tunv <>
2021-05-04 09:51:54 +0700

    init project

---
+++ ChartsDemo-iOS/Swift/Demos/PiePolylineChartViewController.swift
@@ -0,0 +1,181 @@
+//
+//  PiePolylineChartViewController.swift
+//  ChartsDemo-iOS
+//
+//  Created by Jacob Christie on 2017-07-09.
+//  Copyright © 2017 jc. All rights reserved.
+//
+
+import UIKit
+import Charts
+
+class PiePolylineChartViewController: DemoBaseViewController {
+
+    @IBOutlet var chartView: PieChartView!
+    @IBOutlet var sliderX: UISlider!
+    @IBOutlet var sliderY: UISlider!
+    @IBOutlet var sliderTextX: UITextField!
+    @IBOutlet var sliderTextY: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        self.title = "Pie Poly Line Chart"
+        
+        self.options = [.toggleValues,
+                        .toggleXValues,
+                        .togglePercent,
+                        .toggleHole,
+                        .toggleLabelsMinimumAngle,
+                        .animateX,
+                        .animateY,
+                        .animateXY,
+                        .spin,
+                        .drawCenter,
+                        .saveToGallery,
+                        .toggleData]
+        
+        self.setup(pieChartView: chartView)
+        
+        chartView.delegate = self
+        
+        chartView.legend.enabled = false
+        chartView.setExtraOffsets(left: 20, top: 0, right: 20, bottom: 0)
+        chartView.isGetMarkerPosition = true
+
+        sliderX.value = 3
+        sliderY.value = 100
+        self.slidersValueChanged(nil)
+        
+        chartView.animate(xAxisDuration: 1.4, easingOption: .easeOutBack)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            for (index, entry) in ((self.chartView.data?.dataSets.first as? ChartDataSet)?.entries ?? []).enumerated() {
+                if index == 0 {
+                    (entry as? PieChartDataEntry)?.showMarkerLine = true
+                }
+                var width:CGFloat = 0
+                let height:CGFloat = 17
+                if entry.align == .right {
+                    width = 20
+                }
+                let point = CGPoint.init(x: entry.labelPoint.x + self.chartView.frame.origin.x - width, y: entry.labelPoint.y + self.chartView.frame.origin.y - height/2)
+                let subView = UIView.init(frame: CGRect.init(x: point.x, y: point.y, width: 20, height: height))
+//                subView.center = point
+                subView.backgroundColor = UIColor.red
+                
+                self.view.addSubview(subView)
+            }
+            self.chartView.notifyDataSetChanged()
+            self.chartView.notifyDataSetChanged()
+        }
+        
+    }
+    
+    override func updateChartData() {
+        if self.shouldHideData {
+            chartView.data = nil
+            return
+        }
+        
+        self.setDataCount(Int(sliderX.value), range: UInt32(sliderY.value))
+    }
+    
+    func setDataCount(_ count: Int, range: UInt32) {
+        let entries = (0..<count).map { (i) -> PieChartDataEntry in
+            // IMPORTANT: In a PieChart, no values (Entry) should have the same xIndex (even if from different DataSets), since no values can be drawn above each other.
+            let entry = PieChartDataEntry(value: Double(arc4random_uniform(range) + range / 5),
+                                          label: parties[i % parties.count])
+            entry.showMarkerLine = false
+            return entry
+        }
+//        entries.first?.isDrawValuesEnabled = false
+        let set = PieChartDataSet(entries: entries, label: "Election Results")
+        set.sliceSpace = 2
+        set.drawValuesEnabled = false
+        
+        set.colors = ChartColorTemplates.vordiplom()
+            + ChartColorTemplates.joyful()
+            + ChartColorTemplates.colorful()
+            + ChartColorTemplates.liberty()
+            + ChartColorTemplates.pastel()
+            + [UIColor(red: 51/255, green: 181/255, blue: 229/255, alpha: 1)]
+        set.xValuePosition = .outsideSlice
+        set.yValuePosition = .outsideSlice
+//        set.valueLinePart1OffsetPercentage = 0.8
+        set.valueLinePart1Length = 0.4
+        set.valueLinePart2Length = 0.6
+        set.valueLineColor = UIColor.green
+        //set.xValuePosition = .outsideSlice
+        set.yValuePosition = .outsideSlice
+        
+        let data = PieChartData(dataSet: set)
+        
+        let pFormatter = NumberFormatter()
+        pFormatter.numberStyle = .percent
+        pFormatter.maximumFractionDigits = 1
+        pFormatter.multiplier = 1
+        pFormatter.percentSymbol = " %"
+        data.setValueFormatter(DefaultValueFormatter(formatter: pFormatter))
+        data.setValueFont(.systemFont(ofSize: 11, weight: .light))
+        data.setValueTextColor(.black)
+        
+        chartView.data = data
+        chartView.highlightValues(nil)
+    }
+    
+    override func optionTapped(_ option: Option) {
+        switch option {
+        case .toggleXValues:
+            chartView.drawEntryLabelsEnabled = !chartView.drawEntryLabelsEnabled
+            chartView.setNeedsDisplay()
+            
+        case .togglePercent:
+            chartView.usePercentValuesEnabled = !chartView.usePercentValuesEnabled
+            chartView.setNeedsDisplay()
+            
+        case .toggleHole:
+            chartView.drawHoleEnabled = !chartView.drawHoleEnabled
+            chartView.setNeedsDisplay()
+            
+        case .toggleLabelsMinimumAngle:
+            chartView.sliceTextDrawingThreshold = chartView.sliceTextDrawingThreshold == 0.0 ? 20.0 : 0.0
+            chartView.setNeedsDisplay()
+
+        case .drawCenter:
+            chartView.drawCenterTextEnabled = !chartView.drawCenterTextEnabled
+            chartView.setNeedsDisplay()
+            
+        case .animateX:
+            chartView.animate(xAxisDuration: 1.4)
+            
+        case .animateY:
+            chartView.animate(yAxisDuration: 1.4)
+            
+        case .animateXY:
+            chartView.animate(xAxisDuration: 1.4, yAxisDuration: 1.4)
+            
+        case .spin:
+            chartView.spin(duration: 2,
+                           fromAngle: chartView.rotationAngle,
+                           toAngle: chartView.rotationAngle + 360,
+                           easingOption: .easeInCubic)
+            
+        default:
+            handleOption(option, forChartView: chartView)
+        }
+    }
+    
+    // MARK: - Actions
+    @IBAction func slidersValueChanged(_ sender: Any?) {
+        sliderTextX.text = "\(Int(sliderX.value))"
+        sliderTextY.text = "\(Int(sliderY.value))"
+        
+        self.updateChartData()
+    }
+}
